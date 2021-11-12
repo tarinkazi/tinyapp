2cfa68f0bf4ea62bba29f53aaebfaca772c1af73
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-11 23:26:58 -0500

    Added mocha chai test cases

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helper.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+  });
+  it('should return a undefined with invalid email', function() {
+    const user = getUserByEmail("user@example.ca", testUsers)
+    const expectedUserID = "undefined";
+    // Write your assert statement here
+  });
+});
